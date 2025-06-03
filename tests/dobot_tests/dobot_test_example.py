81af7f10561a085d5f84eb3554038ed5baebfd8a
Owen Connell <opc35564@uga.edu>
2025-06-02 20:45:34 -0400

    Move DOBOT tests into tests folder for cleanliness

---
+++ tests/dobot_tests/dobot_test_example.py
@@ -0,0 +1,12 @@
+# Import from external/pydobotplus
+from external.pydobotplus import auto_connect_dobot
+
+def main():
+    dobot = auto_connect_dobot()
+    dobot.speed(velocity=100, acceleration=100)
+    dobot.move_to(200, 0, 0)
+    dobot.home()
+    print("Move complete")
+
+if __name__ == "__main__":
+    main()
